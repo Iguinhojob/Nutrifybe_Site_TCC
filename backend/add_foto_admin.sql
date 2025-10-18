a455d9e0692d9c2bc22aea622ff9144a3c42cc5b
Iguinhojob <igorpinto2811@gmail.com>
2025-10-18 22:27:02 +0000

    Clean up unnecessary files

--- backend/add_foto_admin.sql
+++
@@ -1,6 +0,0 @@
--- Adicionar campo foto na tabela Admin
-USE nutrifybeDB;
-GO
-
-ALTER TABLE Admin ADD foto NVARCHAR(MAX);
-GO
--- clear-data.js
+++
@@ -1,3 +0,0 @@
-// Script para limpar todos os dados do localStorage
-localStorage.clear();
-console.log('Todos os dados foram limpos!');
